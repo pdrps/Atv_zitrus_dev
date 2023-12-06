f31e3e7553898653b665f1ebcabdb0e137ec2c4c
pdrps <126831021+pdrps@users.noreply.github.com>
2023-12-06 12:57:33 -0300

    Update listar_autorizacoes.php

--- listar_autorizacoes.php
+++ listar_autorizacoes.php
@@ -3,7 +3,7 @@
 
     if(!empty($_GET['search'])){
         $data = $_GET['search'];
-        $comando = $pdo->prepare("SELECT * FROM autorizacoes WHERE id_autorizacao LIKE '%$data%' OR procedimento LIKE '%$data%';");
+        $comando = $pdo->prepare("SELECT * FROM autorizacoes WHERE id_autorizacao LIKE '%$data%' OR procedimento LIKE '$data';");
 
         $comando->execute();
         if($comando->rowCount()>= 1)
